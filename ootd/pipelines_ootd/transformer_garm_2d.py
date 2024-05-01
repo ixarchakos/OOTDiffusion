732686b51d5b72c69dde45592e62b0aabd977825
Ioannis Xarchakos <xarchakos@cs.toronto.edu>
2024-05-01 13:53:19 +0300

    commit

--- ootd/pipelines_ootd/transformer_garm_2d.py
+++ ootd/pipelines_ootd/transformer_garm_2d.py
@@ -234,12 +234,12 @@ class Transformer2DModel(ModelMixin, ConfigMixin):
             # TODO(Sayak, PVP) clean this, for now we use sample size to determine whether to use
             # additional conditions until we find better name
             self.adaln_single = AdaLayerNormSingle(inner_dim, use_additional_conditions=self.use_additional_conditions)
-        print("edw237")
-        exit()
+
         self.caption_projection = None
         if caption_channels is not None:
             self.caption_projection = CaptionProjection(in_features=caption_channels, hidden_size=inner_dim)
-
+        print("edw241")
+        exit()
         self.gradient_checkpointing = False
 
     def forward(
