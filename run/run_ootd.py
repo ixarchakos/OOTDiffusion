ecf1532dc67f2e74cbecfbfa1751c9dab54c625a
Ioannis Xarchakos <xarchakos@cs.toronto.edu>
2024-06-19 13:11:53 +0300

    [DEV] king size vton generation

--- run/run_ootd.py
+++ run/run_ootd.py
@@ -196,10 +196,10 @@ def king_size():
                 category = 2
 
             cloth_img = Image.open(get_image_file(image_url)).resize((768, 1024)).convert("RGB")
-            non_transparent = Image.new('RGBA', cloth_img.size, (255, 255, 255))
-            non_transparent.paste(cloth_img, (0, 0), cloth_img)
-            cloth_img = non_transparent
-            cloth_img = cloth_img.resize((768, 1024)).convert("RGB")
+            # non_transparent = Image.new('RGBA', cloth_img.size, (255, 255, 255))
+            # non_transparent.paste(cloth_img, (0, 0), cloth_img)
+            # cloth_img = non_transparent
+            # cloth_img = cloth_img.resize((768, 1024)).convert("RGB")
             cloth_img.save("modified.png")
 
             model_img = Image.open(model_path).resize((768, 1024)).convert("RGB")
